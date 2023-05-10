a2cf99525a60630a79062cf1eb4faed70c8f88f7
klover79 <faiz.kadir.ismail@gmail.com>
2023-05-10 20:55:40 +0800

    Initial Github Load

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}", 
+              "./templates/**/*.{html,js}",
+              "./node_modules/chart.js/**/*.{html,js}",
+            ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
